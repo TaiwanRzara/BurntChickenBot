5f856657944f9c19e024a86796acd4484f00b88d
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-03-05 00:28:39 +0800

    新變更

--- src/cmds/ping.js
+++ src/cmds/ping.js
@@ -2,9 +2,9 @@ import { SlashCommandBuilder } from "discord.js";
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Provides information about the user.'),
+		.setName('ping')
+		.setDescription('!Ping'),
 	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		await interaction.reply(`Pong`);
 	},
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv'
+const fs=require("node:fs");
+const path=require("node:path");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands=new Collection();
 dotenv.config()
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
 client.login(process.env.TestToken);
